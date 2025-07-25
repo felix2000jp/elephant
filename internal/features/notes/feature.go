aa3dfc4657571033bb5dff17ffe21023423fe7e8
felix2000jp <felix2000jp@gmail.com>
2025-07-25 17:59:01 +0100

    move messages to features

--- internal/features/notes/feature.go
+++ internal/features/notes/feature.go
@@ -5,6 +5,21 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+// ListNotesMsg - show the list of notes in the base path
+type ListNotesMsg struct{ Notes []core.Note }
+
+// ViewNoteMsg - select a single note to view and edit
+type ViewNoteMsg struct{ Note core.Note }
+
+// QuitViewNoteMsg - quit the view note state
+type QuitViewNoteMsg struct{}
+
+// EditNoteMsg - enter the edit state for the selected note
+type EditNoteMsg struct{}
+
+// QuitEditNoteMsg - quit the edit note state
+type QuitEditNoteMsg struct{ Note core.Note }
+
 type State int
 
 const (
